900de083de441f89067a1e215308e61849a40415
sava <savamedvedev@gmail.com>
2022-07-01 00:53:34 +0800

    1-commit

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcryptjs");
 const config = require("config");
 const { check, validationResult } = require("express-validator");
 const jwt = require("jsonwebtoken");
+const authMiddleware = require("../middleware/auth.middleware");
 
 const router = new Router();
 
@@ -73,6 +74,28 @@ router.post("/login", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.send({ message: "Server Error" });
+  }
+});
+
+router.get("/auth", authMiddleware, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.user.id });
+    const token = jwt.sign({ id: user.id }, config.get("secretKey"), {
+      expiresIn: "1h",
+    });
+    return res.json({
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        diskSpace: user.diskSpace,
+        usedSpace: user.usedSpace,
+        avatar: user.avatar,
+      },
+    });
+  } catch (e) {
+    console.log(e);
+    res.send({ message: "Server error" });
   }
 });
 
