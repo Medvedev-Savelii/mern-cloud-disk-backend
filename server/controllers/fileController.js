baee6ee77525d66dd0df017067c44c72376837e2
sava <savamedvedev@gmail.com>
2022-07-04 23:55:09 +0800

    15-commit

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -3,7 +3,7 @@ const config = require("config");
 const fs = require("fs");
 const User = require("../models/User");
 const File = require("../models/File");
-
+const Uuid = require("uuid");
 class FileController {
   ////////////////////////////////////////////////////////////
 
@@ -127,9 +127,8 @@ class FileController {
   async downloadFile(req, res) {
     try {
       const file = await File.findOne({ _id: req.query.id, user: req.user.id });
-      const path =
-        (await config.get("filePath")) + "\\" + req.user.id + "\\" + file.path;
-      console.log(path);
+      const path = fileService.getPath(file);
+
       if (fs.existsSync(path)) {
         return res.download(path, file.name);
       }
@@ -170,5 +169,32 @@ class FileController {
     }
   }
   /////////////////////////////////////////////////////////////
+  async uploadAvatar(req, res) {
+    try {
+      const file = req.files.file;
+      const user = await User.findById(req.user.id);
+      const avatarName = Uuid.v4() + ".jpg";
+      file.mv(config.get("staticPath") + "\\" + avatarName);
+      user.avatar = avatarName;
+      await user.save();
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ message: "Upload avatar error" });
+    }
+  }
+  //////////////////////////////////////////////////////////////////
+  async deleteAvatar(req, res) {
+    try {
+      const user = await User.findById(req.user.id);
+      fs.unlinkSync(config.get("staticPath") + "\\" + user.avatar);
+      user.avatar = null;
+      await user.save();
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ message: "Delete avatar error" });
+    }
+  }
 }
 module.exports = new FileController();
